966217bd49fec298fa5959d75982b4e3b286fbbe
M-Manoj-official <manojm1705@outlook.com>
2025-08-22 10:43:20 +0530

    Second commit

--- script.js
+++ script.js
@@ -123,7 +123,9 @@ entryButton.addEventListener('click', () => {
         <td>${subject}</td>
         <td>${credit}</td>
         <td>${grade}</td>
-        <td><button class="remove-btn">Remove</button></td>
+        <td><button class="remove-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+        </svg></button></td>
     `;
     subjectList.appendChild(newRow);
     subjectInput.value = '';
@@ -173,7 +175,7 @@ gpaButton.addEventListener('click', () => {
     const gpa = (totalGradePointsValue / totalCreditsValue).toFixed(2);
     gpapercentage.textContent = `GPA: ${gpa}`;
     totalCredits.textContent = `Total Credits: ${totalCreditsValue}`;
-    totalGradePoints.textContent = `Total Grade Points: ${totalGradePointsValue}`;
+    totalGradePoints.textContent = `Total Grade Points: ${totalGradePointsValue.toFixed(2)}`;
 });
 
 // Saving and loading table data to/from localStorage
@@ -198,7 +200,9 @@ function loadTableData() {
             <td>${item.subject}</td>
             <td>${item.credit}</td>
             <td>${item.grade}</td>
-            <td><button class="remove-btn">Remove</button></td>
+            <td><button class="remove-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+            </svg></button></td>
         `;
         subjectList.appendChild(newRow);
     });
